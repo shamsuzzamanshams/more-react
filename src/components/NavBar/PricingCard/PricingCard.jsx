240f5a2f1e524b15293554094e5831909d4b23e1
shamsuzzamanshams <shams30200@gmail.com>
2026-03-10 01:04:55 +0600

    create custom card and work in daisy ui card

--- src/components/NavBar/PricingCard/PricingCard.jsx
+++ src/components/NavBar/PricingCard/PricingCard.jsx
@@ -5,21 +5,22 @@ const PricingCard = ({ pricing }) => {
 	const { planName, price, description, features } = pricing;
 
 	return (
-		<div className='border bg-sky-600 rounded-2xl p-4'>
+		<div className='flex flex-col border bg-sky-600 rounded-2xl p-4'>
 			{/* card header */}
 			<div>
 				<h1 className='text-5xl'>{planName}</h1>
 				<h4 className='text-3xl'>{price}</h4>
 			</div>
 			{/* card body */}
-			<div className='bg-sky-400 p-4 rounded-2xl mt-10'>
+			<div className='bg-sky-400 p-4 rounded-2xl mt-10 flex-1'>
 				<p>{description}</p>
 				{
-					features.map((feature, index )=> <PricingFeatures 
-					key={index}
-					feature={feature}></PricingFeatures>)
+					features.map((feature, index) => <PricingFeatures
+						key={index}
+						feature={feature}></PricingFeatures>)
 				}
 			</div>
+			<button className="btn w-full bg-black text-white mt-4">Subscribe</button>
 		</div>
 	);
 };
--- src/components/NavBar/PricingOptions/PricingOptions.jsx
+++ src/components/NavBar/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPeicingData from '../daisyPricingData/daisyPeicingData';
 
 const PricingOptions = ({ pricingOption }) => {
 	const pricingData = use(pricingOption)
@@ -8,11 +9,17 @@ const PricingOptions = ({ pricingOption }) => {
 	return (
 		<div>
 			<h2 className='text-5xl'>Get Our Membership</h2>
-			<div className='grid md:grid-cols-3 gap-8'>
-				{
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+				{/* {
 					pricingData.map(pricing => <PricingCard
 						key={pricing.id}
 						pricing={pricing}></PricingCard>)
+				} */}
+				{
+					pricingData.map(pricing => <DaisyPeicingData
+						key={pricing.id}
+						pricing={pricing}
+					></DaisyPeicingData>)
 				}
 			</div>
 		</div>
